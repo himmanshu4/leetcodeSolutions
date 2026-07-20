44f0d7c2c96439859bedcf60a4e97be2f4a98a17
Himanshu Mishra <77191140+himmanshu4@users.noreply.github.com>
2026-07-20 20:02:10 +0530

    Time: 0 ms (100%), Space: 8.7 MB (12.36%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    double myPow(double x, long long int n ) {
+    double myPow(double x, long long int n, double ans=1 ) {
         if( n==0 ){
-            return 1;
+            return ans;
         }
         if(n<0){
             n*=-1;
             x=1/x;
         }
         if(n&1){
-            return x*myPow(x*x,n/2);
+            return myPow(x*x,n/2,ans*x);
         }
         else{
-            return myPow(x*x,n/2);
+            return myPow(x*x,n/2,ans);
         }
     }
 };
